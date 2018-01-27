69ba1f0e80023bd192051cef72f705f3b7c16b27
jordan9827 <neeraj.rathore596@gmail.com>
2018-01-27 11:05:49 +0530

    add dependency

--- GitHubTestApp/app/src/main/java/com/thinking/machines/githubtestapp/MainActivity.java
+++ GitHubTestApp/app/src/main/java/com/thinking/machines/githubtestapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.thinking.machines.githubtestapp;
 
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity {
+  //  D:\GitProjects\mob\GitHubTestApp\app
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
